5eb951f3a8d451f76106c3faa445af6952a12749
Charlie Moad <cmoad@exacttarget.com>
2013-11-07 11:45:27 -0500

    added amazons3 and heroku site

--- sites/heroku/settings.php
+++ sites/heroku/settings.php
@@ -13,5 +13,5 @@ $conf['404_fast_paths'] = '/\.(?:txt|png|gif|jpe?g|css|js|ico|swf|flv|cgi|bat|pl
 $conf['404_fast_html'] = '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML+RDFa 1.0//EN" "http://www.w3.org/MarkUp/DTD/xhtml-rdfa-1.dtd"><html xmlns="http://www.w3.org/1999/xhtml"><head><title>404 Not Found</title></head><body><h1>Not Found</h1><p>The requested URL "@path" was not found on this server.</p></body></html>';
 
 // Begin Heroku specific configuration
-require_once('./heroku.inc');
+require_once(DRUPAL_ROOT . '/heroku.inc');
 $databases = Heroku_Util::parseDatabaseUrl();
