e6abe5a19243c6509a779d656d0f4ff9a019b93e
Charlie Moad <cmoad@exacttarget.com>
2013-11-07 11:32:42 -0500

    added heroku autoconfig

---
+++ sites/default/settings.php
@@ -0,0 +1,17 @@
+<?php
+
+$update_free_access = FALSE;
+$drupal_hash_salt = '';
+
+ini_set('session.gc_probability', 1);
+ini_set('session.gc_divisor', 100);
+ini_set('session.gc_maxlifetime', 200000);
+ini_set('session.cookie_lifetime', 2000000);
+
+$conf['404_fast_paths_exclude'] = '/\/(?:styles)\//';
+$conf['404_fast_paths'] = '/\.(?:txt|png|gif|jpe?g|css|js|ico|swf|flv|cgi|bat|pl|dll|exe|asp)$/i';
+$conf['404_fast_html'] = '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML+RDFa 1.0//EN" "http://www.w3.org/MarkUp/DTD/xhtml-rdfa-1.dtd"><html xmlns="http://www.w3.org/1999/xhtml"><head><title>404 Not Found</title></head><body><h1>Not Found</h1><p>The requested URL "@path" was not found on this server.</p></body></html>';
+
+// Begin Heroku specific configuration
+require_once('./heroku.inc');
+$databases = Heroku_Util::parseDatabaseUrl();
